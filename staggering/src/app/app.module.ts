058d26cb12d508ba18e320db19a24a31eb39171b
Nguyen Duc Manh <manhndgch16236@fpt.edu.vn>
2018-09-07 13:44:50 +0700

    add path

--- staggering/src/app/app.module.ts
+++ staggering/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule, Routes} from "@angular/router";
 
+const routes: Routes = [{
+  path: '/',component:AppComponent
+}];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
